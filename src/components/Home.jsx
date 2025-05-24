ef48a50674156e6b36ce96a8068a5989fddc46e8
beastkasper <62433555+beastkasper@users.noreply.github.com>
2025-05-24 19:18:29 +0500

    add new word

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home({ onStart }) {
   return (
     <div className="home-screen">
       <h1 className="title">Quizzy Land</h1>
-      <p className="subtitle">Готов к весёлому челленджу по фронтенду?</p>
+      <p className="subtitle">Готов к весёлому челленджу по фронтенду? Да капитан! </p>
       <button className="start-button" onClick={onStart}>Начать игру</button>
     </div>
   );
